5a4a54a66a09a4fc2367c7a6a4e8d1212dd09008
Adailson Filho <adailsonfilho@gmail.com>
2016-05-16 19:08:02 -0300

    correcao de tabulacao

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import ipdb
 
 # Use the CPU in 64-bit mode.
 # from sknn.platform import cpu64
@@ -62,29 +61,29 @@ if __name__ == '__main__':
     # sm = SMOTE(kind='svm', verbose=verbose, **svm_args)
     # svmx, svmy = sm.fit_transform(classe0, classe1)
 
-    '''SPLITING DATA'''
+    # '''SPLITING DATA'''
 
     #rondomize classe 0
-    np.random.shuffle(svmx)
+	np.random.shuffle(svmx)
 
     #rondomize classe 1
-    np.random.shuffle(svmy)
+	np.random.shuffle(svmy)
 
-    #Prieira metade - 50%
-    halfx = int((len(svmx)*.5))
-    halfy = int((len(svmy)*.5))
-    trainingsetx = svmx[0:halfx]
-    trainingsety = svmy[0:halfy]
+	#Prieira metade - 50%
+	halfx = int((len(svmx)*.5))
+	halfy = int((len(svmy)*.5))
+	trainingsetx = svmx[0:halfx]
+	trainingsety = svmy[0:halfy]
 
-    #Prieiro quarto - 25%
-    quarter1x = int((len(svmy)*.75))
-    quarter1y = int((len(svmy)*.75))
-    validationsetx = svmx[halfx:quarter1x]
-    validationsety = svmy[halfy:quarter1y]
+	#Prieiro quarto - 25%
+	quarter1x = int((len(svmy)*.75))
+	quarter1y = int((len(svmy)*.75))
+	validationsetx = svmx[halfx:quarter1x]
+	validationsety = svmy[halfy:quarter1y]
 
-    #Segundo quarto - 25%
-    testsetx = svmx[quarter1x:]
-    testsety = svmy[quarter1y:]
+	#Segundo quarto - 25%
+	testsetx = svmx[quarter1x:]
+	testsety = svmy[quarter1y:]
 
 
 	'''LEARNING'''
