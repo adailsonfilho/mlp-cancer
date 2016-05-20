4e5617be61b002392f81ca211143f805761bd095
matheus dornelas <matheus dornelas>
2016-05-20 07:00:16 -0300

    Modificando a divisão para o oversampling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import csv as libcsv
 # Use the CPU in 64-bit mode.
 # from sknn.platform import cpu64
 from sknn.mlp import Layer, Classifier
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 	    	line_ = [ float(val) for val in line.split(',')]
 
 	    	#for united data
-	    	data.append(line_[:-1])
-	    	target.append(int(line_[-1]))
+	    	# data.append(line_[:-1])
+	    	# target.append(int(line_[-1]))
 
 	    	#for separeted data
 	    	if line_[-1] == 0:
@@ -32,58 +32,72 @@ if __name__ == '__main__':
 
 	classe0 = np.array(classe0)
 	classe1 = np.array(classe1)
-	data = np.array(data)
-	target = np.array(target)
+	# data = np.array(data)
+	# target = np.array(target)
 
 	print('Classe 0 size:',len(classe0))
 	print('Classe 1 size:',len(classe1))
 
-	
+	#randomize classe 0
+	np.random.shuffle(classe0)
+
+    #randomize classe 1
+	np.random.shuffle(classe1)
+
+	#Preparando conjunto de treinamento - 50%
+	halfx = int((len(classe0)*.5))
+	halfy = int((len(classe1)*.5))
+
+	trainingsetx = classe0[0:halfx]
+	trainingsety = classe1[0:halfy]
+
+	print('Classe 0 training size:',len(trainingsetx))
+	print('Classe 1 training size:',len(trainingsety))
+
 	'''OVERSAMPLING'''
 	
-	sm = SMOTE(kind='regular', verbose='verbose')
-	svmx, svmy = sm.fit_transform(data, target)
+	sm = SMOTE(kind='regular', verbose=True)
+	svmx, svmy = sm.fit_transform(trainingsetx, trainingsety)
 
-	print("Dados balanceados")
-	print("Tamanho da classe 0",len(svmx))
-	print("Tamanho da classe 1",len(svmy))
+	print('Classe 0 balanced training size:',len(svmx))
+	print('Classe 1 balanced training size:',len(svmy))
 
-    # print('SMOTE bordeline 1')
-    # sm = SMOTE(kind='borderline1', verbose=verbose)
-    # svmx, svmy = sm.fit_transform(classe0, classe1)
+######################################################################################
 
-    # print('SMOTE bordeline 2')
-    # sm = SMOTE(kind='borderline2', verbose=verbose)
-    # svmx, svmy = sm.fit_transform(classe0, classe1)
+	#Primeiro quarto - conjunto de validação - 25%
+	quarter1x = int((len(classe0)*.75))
+	quarter1y = int((len(classe1)*.75))
 
-    # print('SMOTE SVM')
-    # svm_args={'class_weight': 'auto'}
-    # sm = SMOTE(kind='svm', verbose=verbose, **svm_args)
-    # svmx, svmy = sm.fit_transform(classe0, classe1)
+	validationsetx = classe0[halfx:quarter1x]
+	validationsety = classe1[halfy:quarter1y]
 
-    # '''SPLITING DATA'''
+	print('Classe 0 validation size:',len(validationsetx))
+	print('Classe 1 validation size:',len(validationsety))
 
-    #rondomize classe 0
-	np.random.shuffle(svmx)
+	'''OVERSAMPLING'''
+	
+	sm = SMOTE(kind='regular', verbose=True)
+	svmx, svmy = sm.fit_transform(validationsetx, validationsety)	
 
-    #rondomize classe 1
-	np.random.shuffle(svmy)
+	print('Classe 0 balanced validation size:',len(svmx))
+	print('Classe 1 balanced validation size:',len(svmy))	
 
-	#Prieira metade - 50%
-	halfx = int((len(svmx)*.5))
-	halfy = int((len(svmy)*.5))
-	trainingsetx = svmx[0:halfx]
-	trainingsety = svmy[0:halfy]
+#######################################################################################
 
-	#Prieiro quarto - 25%
-	quarter1x = int((len(svmy)*.75))
-	quarter1y = int((len(svmy)*.75))
-	validationsetx = svmx[halfx:quarter1x]
-	validationsety = svmy[halfy:quarter1y]
+	#Segundo quarto - conjunto de teste - 25%
+	testsetx = classe0[quarter1x:]
+	testsety = classe1[quarter1y:]
 
-	#Segundo quarto - 25%
-	testsetx = svmx[quarter1x:]
-	testsety = svmy[quarter1y:]
+	print('Classe 0 test size:',len(testsetx))
+	print('Classe 1 test size:',len(testsety))
+
+	'''OVERSAMPLING'''
+
+	sm = SMOTE(kind='regular', verbose=True)
+	svmx, svmy = sm.fit_transform(validationsetx, validationsety)	
+
+	print('Classe 0 balanced validation size:',len(svmx))
+	print('Classe 1 balanced validation size:',len(svmy))
 
 
 	'''LEARNING'''
