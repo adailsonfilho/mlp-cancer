9573374bdf29049a1a1ff055022c97f0e44f7181
Adailson de Castro <Adailson de Castro>
2016-05-25 02:07:31 -0300

    treinando e classificando

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-import csv as libcsv
+# import csv as libcsv
+import ipdb
 # Use the CPU in 64-bit mode.
 # from sknn.platform import cpu64
 from sknn.mlp import Layer, Classifier
@@ -36,6 +37,8 @@ if __name__ == '__main__':
     #randomize classe 1
 	np.random.shuffle(classe1)
 
+##################################################################################
+
 	#Preparando conjunto de treinamento - 50%
 	halfx = int((len(classe0)*.5))
 	halfy = int((len(classe1)*.5))
@@ -45,6 +48,8 @@ if __name__ == '__main__':
 
 	data = np.concatenate((trainingsetx,trainingsety))
 	target = np.concatenate((classe0[:halfx,-1],classe1[:halfy,-1]))
+
+	print('>> Training')
 
 	print('Classe 0 training size:',len(trainingsetx))
 	print('Classe 1 training size:',len(trainingsety))
@@ -67,51 +72,67 @@ if __name__ == '__main__':
 	validationsetx = classe0[halfx:quarter1x,:-1]
 	validationsety = classe1[halfy:quarter1y,:-1]
 
-	data = np.concatenate((validationsetx,validationsety))
-	target = np.concatenate((classe0[halfx:quarter1x,-1],classe1[halfy:quarter1y,-1]))
+	vdata = np.concatenate((validationsetx,validationsety))
+	vtarget = np.concatenate((classe0[halfx:quarter1x,-1],classe1[halfy:quarter1y,-1]))
 
-	print('Validation size:',len(validationsetx))
+	print('>> Validation size:',len(validationsetx))
 
 	'''OVERSAMPLING'''
 	
-	sm = SMOTE(kind='regular', verbose=True)
-	balancedValidationSetData, balancedValidationSetTarget = sm.fit_transform(data, target)
+	sm = SMOTE(kind='regular')
+	balancedValidationSetData, balancedValidationSetTarget = sm.fit_transform(vdata, vtarget)
 
 	print('Balanced validation size:',len(balancedValidationSetData))
+	print('Balanced validation data shape:',balancedValidationSetData.shape)
+	print('Balanced validation target shape:',balancedValidationSetTarget.shape)
 
 #######################################################################################
 
 	#Segundo quarto - conjunto de teste - 25%
-	testsetx = classe0[quarter1x:]
-	testsety = classe1[quarter1y:]
+	testsetx = classe0[quarter1x:,:-1]
+	testsety = classe1[quarter1y:,:-1]
 
-	data = np.concatenate((testsetx,testsety))
-	target = np.concatenate((classe0[quarter1x:,-1],classe1[quarter1y:,-1]))
+	tdata = np.concatenate((testsetx,testsety))
+	ttarget = np.concatenate((classe0[quarter1x:,-1],classe1[quarter1y:,-1]))
 
+	print('>> Test')
 	print('Classe 0 test size:',len(testsetx))
 	print('Classe 1 test size:',len(testsety))
 
-	'''OVERSAMPLING'''
-
-	sm = SMOTE(kind='regular', verbose=True)
-	balancedTestSetData, balancedTestSetTarget = sm.fit_transform(data, target)
+	balancedTestSetData = tdata
+	balancedTestSetTarget = ttarget
 
 	print('Balanced test size:',len(balancedTestSetData))
 
 	'''LEARNING'''
-	inputlayer = Layer(type='Sigmoid',name="input_layer_1",units=6)
-	hiddenlayer1 = Layer(type='Sigmoid',name="hidden_layer_1",units=4)
-	outputlayer = Layer(type='Softmax',name="output_layer",units=2)
+	hiddenlayer1 = Layer(type='Sigmoid',name="input_layer_1",units=4)
+	hiddenlayer2 = Layer(type='Sigmoid',name="hidden_layer_1",units=3)
+	outputlayer = Layer(type='Softmax',name="output_layer")
 
+	print('Initializing cliassifier')
 	nn = Classifier(
 	    layers=[
-	    	inputlayer,
-	    	hiddenlayer1,
+	    	# hiddenlayer1,
+	    	hiddenlayer2,
 			outputlayer],
 	    learning_rate=0.001,
-	    n_iter=100)
+	    n_iter=20,
+	    valid_set=(balancedValidationSetData,balancedValidationSetTarget),
+	    verbose = True
+	    )
 
+	print('Fitting')
 	nn.fit(balancedTrainingSetData,balancedTrainingSetTarget)
+
+	print('Testing')
+	errors = 0
+	for sample,obj in zip(data,target):
+		 if nn.predict(sample) != obj:
+		 	errors+= 1
+
+	print("acurracy:", ((len(data)-errors)/len(data))*100,'%')
+	print('errors',errors,'of', len(data))
+
 
 	# clf = MLPClassifier(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(5, 1), random_state=1, shuffle=True)
 	# clf.fit(classe0, classe1)
