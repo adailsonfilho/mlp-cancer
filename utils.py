bdceed4ed8dec54c93baa8342351ee42bda3fab7
Jonathan Santos <santojon5@gmail.com>
2016-05-30 18:33:43 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,27 +4,8 @@ import os
 import matplotlib.pyplot as plt
 
 def save(fname, ext='png', close=True, verbose=True):
-    """Save a figure from pyplot.
-    Parameters
-    ----------
-    path : string
-        The path (and filename, without the extension) to save the
-        figure to.
-    ext : string (default='png')
-        The file extension. This must be supported by the active
-        matplotlib backend (see matplotlib.backends module).  Most
-        backends support 'png', 'pdf', 'ps', 'eps', and 'svg'.
-    close : boolean (default=True)
-        Whether to close the figure after saving.  If you want to save
-        the figure multiple times (e.g., to multiple formats), you
-        should NOT close it in between saves or you will have to
-        re-plot it.
-    verbose : boolean (default=True)
-        Whether to print information about when and where the image
-        has been saved.
-    """
     
-    # Extract the directory and filename from the given path
+    # Set the directory and filename
     directory = 'results'
     filename = "%s.%s" % (fname, ext)
     if directory == '':
